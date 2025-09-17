a610a49f6befcd0b8f8306c4013276d251497d12
Serghei28 <serghei.petis@isa.utm.md>
2025-09-17 13:21:35 +0300

    Orders Entity (Admin)

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,10 +1,12 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@/lib/generated/prisma";  // 👈 вместо @prisma/client
 
 declare global {
-    var prisma: PrismaClient | undefined
-};
+  // Чтобы не было лишних подключений в dev
+  var prisma: PrismaClient | undefined;
+}
 
 const prismadb = globalThis.prisma || new PrismaClient();
+
 if (process.env.NODE_ENV !== "production") globalThis.prisma = prismadb;
 
 export default prismadb;
